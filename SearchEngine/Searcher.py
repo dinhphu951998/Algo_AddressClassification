5cc3e15b65d70b26bba3c8b478f41873649ad41d
Phu Nguyen <dinhphu951998@gmail.com>
2025-03-28 01:59:54 +0700

    Add run tests

--- SearchEngine/Searcher.py
+++ SearchEngine/Searcher.py
@@ -1,6 +1,3 @@
-from re import search
-
-import editdistance
 
 class Searcher:
     def __init__(self, possible_provinces, possible_districts, possible_wards, province_trie, district_trie, ward_trie):
@@ -12,66 +9,36 @@ class Searcher:
         self.ward_trie = ward_trie
 
     def process(self):
-        final_province = self.search_part(self.possible_provinces, self.province_trie, "province")
-        print(final_province)
-        final_district = self.search_part(self.possible_districts, self.district_trie, "district")
-        print(final_district)
-        final_ward = self.search_part(self.possible_wards, self.ward_trie, "ward")
-        print(final_ward)
+        n = len(self.possible_provinces)
+
+        district = ""
+        ward = ""
+
+        next_index, province = self.search_part(self.possible_provinces, self.province_trie, n - 1)
+
+        if province:
+            next_index, district = self.search_part(self.possible_districts, self.district_trie, next_index)
+
+        if district:
+            next_index, ward = self.search_part(self.possible_wards, self.ward_trie, next_index)
 
         return {
-            "province": final_province["best_candidate"],
-            "district": final_district["best_candidate"],
-            "ward": final_ward["best_candidate"]
+            "province": province,
+            "district": district,
+            "ward": ward
         }
 
-    part_possibilities = {
-        "province": 1,
-        "district": 2/3,
-        "ward": 1/3
-    }
 
-    def search_part(self, possible_words, trie, type):
-        result = []
-        n = len(possible_words)
-
-        for i, words_at_i in enumerate(possible_words):
+    def search_part(self, possible_words, trie, i):
+        while i >= 0:
+            words_at_i = possible_words[i]
             if len(words_at_i) == 0:
+                i -= 1
                 continue
-            calculator = self.search(words_at_i, trie)
-            calculator["distance"] = calculator["distance"] * abs(i - self.part_possibilities[type] * n + 1) / n
-            result.append(calculator)
 
-        result.sort(key=lambda x: x["distance"])
-        return result[0]
+            words_at_i.sort(key=lambda x: len(x), reverse=True)
+            candidates = trie.search_for_normalized_result(words_at_i[0])
+            return i - len(words_at_i[0]) + 1, candidates[0][2]
+        return 0, ""
 
-
-    def search(self, words, trie):
-        words.sort(key=lambda x: len(x), reverse=True)
-        all_candidates = []
-        for word in words:
-            candidates = trie.search_for_normalized_result(word)
-            all_candidates = all_candidates + candidates
-        if len(all_candidates) == 0:
-            return {
-                "best_candidate": "",
-                "key": "",
-                "distance": float("inf")
-            }
-        return self.find_best_candidates(words, all_candidates)
-
-    def find_best_candidates(self, word, candidates):
-        min_distance = float("inf")
-        best_candidate = None
-        for candidate in candidates:
-            distance = editdistance.distance(word, candidate[0])
-            if distance < min_distance:
-                min_distance = distance
-                best_candidate = candidate
-
-        return {
-            "best_candidate": best_candidate[2],
-            "key": best_candidate[1],
-            "distance": min_distance
-        }
 
--- Solution.py
+++ Solution.py
@@ -14,9 +14,13 @@ class Solution:
 
     def __init__(self):
         # list province, district, ward for private test, do not change for any reason (these file will be provided later with this exact name)
-        self.province_path = 'vietnamese-provinces-database/list_province.txt'
-        self.district_path = 'vietnamese-provinces-database/list_district.txt'
-        self.ward_path = 'vietnamese-provinces-database/list_ward.txt'
+        # self.province_path = 'vietnamese-provinces-database/list_province.txt'
+        # self.district_path = 'vietnamese-provinces-database/list_district.txt'
+        # self.ward_path = 'vietnamese-provinces-database/list_ward.txt'
+
+        self.province_path = 'list_province.txt'
+        self.district_path = 'list_district.txt'
+        self.ward_path = 'list_ward.txt'
 
         # Index Analyzer
         start = time.time()  # current time in seconds
@@ -43,14 +47,15 @@ class Solution:
         possible_districts = search_analyzer.find_matching_words(str, self.trie_district)
         possible_wards = search_analyzer.find_matching_words(str, self.trie_ward)
 
-        print("Possible provinces: ", possible_provinces)
-        print("Possible districts: ", possible_districts)
-        print("Possible wards: ", possible_wards)
-
         # find best candidates
         searcher = Searcher(possible_provinces, possible_districts, possible_wards, self.trie_province, self.trie_district, self.trie_ward)
         result = searcher.process()
-        print("Result: ", result)
+
+        # print("Possible provinces: ", possible_provinces)
+        # print("Possible districts: ", possible_districts)
+        # print("Possible wards: ", possible_wards)
+        # print("Result: ", result)
+
         print()
 
         return result
@@ -58,7 +63,8 @@ class Solution:
 # Test
 runner = Solution()
 runner.process("284DBis Ng Văn Giáo, P3, Mỹ Tho, T.Giang.")
-runner.process("357/28,Ng-T- Thuật,P1,Q3,TP.HồChíMinh.")
-runner.process("Khu phố 4 Thị trấn, Dương Minh Châu, Tây Ninh")
-runner.process("A:12A.21BlockA C/c BCA,P.AnKhánh,TP.Thủ Đức, TP. HCM")
-runner.process("Hải Ba  Hải Lăng ")
+runner.process("Tiểu khu 3, thị trấn Ba Hàng, huyện Phổ Yên, tỉnh Thái Nguyên.")
+# runner.process("357/28,Ng-T- Thuật,P1,Q3,TP.HồChíMinh.")
+# runner.process("Khu phố 4 Thị trấn, Dương Minh Châu, Tây Ninh")
+# runner.process("A:12A.21BlockA C/c BCA,P.AnKhánh,TP.Thủ Đức, TP. HCM")
+# runner.process("Hải Ba  Hải Lăng ")
